c786275b761272a6938ea13ddbd00dfe17e6db05
leonardost <leonardost@users.noreply.github.com>
2019-07-16 08:37:13 -0300

    Comment some debug statements and remove max 4 Harris corner candidates restriction

--- app/src/main/java/br/edu/ifspsaocarlos/sdm/kifurecorder/processamento/cornerDetector/CornerDetector.java
+++ app/src/main/java/br/edu/ifspsaocarlos/sdm/kifurecorder/processamento/cornerDetector/CornerDetector.java
@@ -11,17 +11,29 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import br.edu.ifspsaocarlos.sdm.kifurecorder.processamento.FileHelper;
+import br.edu.ifspsaocarlos.sdm.kifurecorder.processamento.Logger;
 import br.edu.ifspsaocarlos.sdm.kifurecorder.processamento.Ponto;
 
 public class CornerDetector {
 
     public final static int RADIUS_OF_REGION_OF_INTEREST = 40;
-    private int imageIndex;
+    private long imageIndex;
     private int cornerIndex;
     private Corner corner;
+    private FileHelper fileHelper;
+    private Logger logger;
 
     private HarrisCornerDetector harrisCornerDetector = new HarrisCornerDetector();
     private EllipseCornerDetector ellipseCornerDetector = new EllipseCornerDetector();
+
+    public void setFileHelper(FileHelper fileHelper) {
+        this.fileHelper = fileHelper;
+    }
+
+    public void setLogger(Logger logger) {
+        this.logger = logger;
+    }
 
     public void setImageIndex(int imageIndex) {
         this.imageIndex = imageIndex;
@@ -42,7 +54,7 @@ public class CornerDetector {
     public Corner detectCornerIn(Mat image) {
         System.out.println("Processing corner " + cornerIndex);
         Mat regionImage = getRegionOfInterestAround(corner, image);
-        Imgcodecs.imwrite("processing/corner" + cornerIndex + "_frame" + imageIndex + ".png", regionImage);
+//        Imgcodecs.imwrite("processing/corner" + cornerIndex + "_frame" + imageIndex + ".png", regionImage);
 
         List<Corner> candidateCornerHarris = harrisCornerDetector.detectCandidateCornersIn(regionImage);
         List<Corner> candidateCornerEllipsis = ellipseCornerDetector.detectCandidateCornersIn(regionImage);
@@ -72,16 +84,19 @@ public class CornerDetector {
             System.out.println(point);
             Imgproc.circle(imageWithCornersPlotted, new Point(point.getX(), point.getY()), 1, new Scalar(0, 0, 255), -1);
         }
-        Imgcodecs.imwrite("processing/corner" + cornerIndex + "_frame" + imageIndex + "_candidate_corners.png", imageWithCornersPlotted);
+//        Imgcodecs.imwrite("processing/corner" + cornerIndex + "_frame" + imageIndex + "_candidate_corners.png", imageWithCornersPlotted);
+        fileHelper.writePngImage(imageWithCornersPlotted, "corner" + cornerIndex + "_frame" + imageIndex + "_candidate_corners");
+//        fileHelper.writePngImage(imageWithCornersPlotted, Imgproc.COLOR_RGBA2BGR, "corner" + cornerIndex + "_frame" + imageIndex + "_candidate_corners");
 
         List<Corner> candidateCorners = new ArrayList<>();
         candidateCorners.addAll(candidateCornerHarris);
         candidateCorners.addAll(candidateCornerEllipsis);
+//        filterCandidateCornersWhichFallOutsideROI(cnadidateCorners);
 
         // A corner should have at most 4 candidates, be them Harris corners or ellipsis corners
         // More than that probably means something is wrong in the detection, or there's something
         // else in the scene, like a player's hand or something else
-        if (candidateCorners.size() > 4) return null;
+//        if (candidateCorners.size() > 4) return null;
 
         Corner candidateCorner = getCandidateNearestToCenterOfRegionOfInterest(candidateCorners);
 
@@ -104,6 +119,14 @@ public class CornerDetector {
         Rect regionOfInterest = new Rect(x, y, w, h);
         return new Mat(image, regionOfInterest);
     }
+
+//    private void filterCandidateCornersWhichFallOutsideROI(List<Corner> candidateCorners) {
+//        Iterator it = candidateCorners.iterator();
+//        while (it.hasNext()) {
+//            Corner candidateCorner = (Corner)it.next();
+//            if (candidateCorner.getX())
+//        }
+//    }
 
     private Corner getCandidateNearestToCenterOfRegionOfInterest(List<Corner> corners) {
         Ponto center = new Ponto(RADIUS_OF_REGION_OF_INTEREST, RADIUS_OF_REGION_OF_INTEREST);
