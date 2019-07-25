a3a63d4db32ac51cda26bfcdbd67fc2ebbe45f07
leonardost <leonardost@users.noreply.github.com>
2019-07-24 23:21:29 -0300

    Create rotateImage method in TransformadorDeTabuleiro

--- app/src/main/java/br/edu/ifspsaocarlos/sdm/kifurecorder/processamento/TransformadorDeTabuleiro.java
+++ app/src/main/java/br/edu/ifspsaocarlos/sdm/kifurecorder/processamento/TransformadorDeTabuleiro.java
@@ -2,6 +2,8 @@ package br.edu.ifspsaocarlos.sdm.kifurecorder.processamento;
 
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 
 /**
@@ -27,6 +29,17 @@ public class TransformadorDeTabuleiro {
         Imgproc.warpPerspective(imagemOriginal, tabuleiroOrtogonal, matrizDeTransformacao, tabuleiroOrtogonal.size());
 
         return tabuleiroOrtogonal;
+    }
+
+    // direction = -1 counter-clockwise, 1 clockwise
+    public static Mat rotateImage(Mat image, int direction) {
+        Point center = new Point(image.cols() / 2, image.rows() / 2);
+        direction *= -1;
+        // Positive values mean counter-clockwise rotation
+        Mat transformationMatrix = Imgproc.getRotationMatrix2D(center, 90 * direction, 1);
+        Mat rotatedImage = new Mat();
+        Imgproc.warpAffine(image, rotatedImage, transformationMatrix, new Size(image.cols(), image.rows()));
+        return rotatedImage;
     }
 
 }
